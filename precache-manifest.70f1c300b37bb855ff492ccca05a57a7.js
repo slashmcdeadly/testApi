c434fecea116c4d47e7622c20bd4b6e5b5343a20
Scott Carter <scottandrewcarter1@hotmail.com>
2020-06-26 11:47:36 -0400

    Updates

---
+++ precache-manifest.70f1c300b37bb855ff492ccca05a57a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479f92a4cb499fff1e99a807ec50a64f",
+    "url": "/testApi/index.html"
+  },
+  {
+    "revision": "2d4da56a672e2b8dbd53",
+    "url": "/testApi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a2cb12d5dd785263eed",
+    "url": "/testApi/static/js/2.c6fec889.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testApi/static/js/2.c6fec889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4da56a672e2b8dbd53",
+    "url": "/testApi/static/js/main.b0f78ecb.chunk.js"
+  },
+  {
+    "revision": "295ca28c2955add56741",
+    "url": "/testApi/static/js/runtime-main.af3d5e9e.js"
+  }
+]);
